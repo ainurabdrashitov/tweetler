9d3b19eefb3c0045bd097360aa3a0241c9e84b71
Ainur Abdrashitov <smithunknownslv@gmail.com>
2022-11-19 21:01:32 +0300

    Add libs for REST testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion = "2.1.3"
+val jacksonDatatypeVersion = "2.14.0"
+val logbackClassicVersion = "1.4.4"
+val assertjVersion = "3.23.1"
+val restAssuredVersion = "5.2.0"
+val junitVersion = "5.9.0"
 
 plugins {
     kotlin("jvm") version "1.7.21"
@@ -19,9 +24,12 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
-    testImplementation(kotlin("test"))
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonDatatypeVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackClassicVersion")
+    testImplementation("org.assertj:assertj-core:$assertjVersion")
+    testImplementation("io.rest-assured:rest-assured:$restAssuredVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 tasks.test {
